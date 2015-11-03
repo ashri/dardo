09825a3740c69a0310b4d47db6112cb81c725e11
Ashley Richardson <ash@threeheadedmonkey.com>
2015-11-03 10:30:12 +0800

    Switch to use Polymer for the front end framework

---
+++ web/index.dart
@@ -0,0 +1,11 @@
+library dardo.web.index;
+
+import 'package:dardo/main_app.dart';
+import 'package:polymer/polymer.dart';
+
+/// [MainApp] used!
+main() async {
+  await initPolymer();
+
+  print("Ready to go!");
+}
